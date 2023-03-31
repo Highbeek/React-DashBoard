f8077b8f96690c3e7d6a39ca6d6a7147704c9920
Ibukun <ibukunagbo@gmail.com>
2023-03-31 10:23:16 +0100

    finished the data section of the dashboard

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { ColorModeContext, useMode } from "../themes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./scenes/global/TopBar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Contact from "./scenes/contacts";
+
+import Invoices from "./scenes/invoices";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Pie from "./scenes/pie";
+// import Line from "./scenes/line";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contact />} />
+
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
